3995701002143406212b79bec07a1ba00de92195
maurennys4 <velasquezlisay2@gmail.com>
2023-07-05 05:19:12 -0500

    nuevas modificaciones

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-let.addEventListener("load",function(){
-document.getElementById("cambiarColor").addEventListener("click",function(){       
-var colorFondo = document.addEventListener("colorFondo").value;
-document.addEventListener("cuerpo").setAtribute(bgcolor,colorFondo)
-    })
+ const btn=document.querySelector('button');
+ const body=document.querySelector('body');
 
-   }) 
+ const colores=['blue','yellow','green','white'];
+
+ btn .addEventListener('click',fondo);
+ function fondo(){
+    const indice=parseInt(Math.random()*colores.length);
+    body.style.backgrounColor=colores[indice];
+ }
 
     
 
